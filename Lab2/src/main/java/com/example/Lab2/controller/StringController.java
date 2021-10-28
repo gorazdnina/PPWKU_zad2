b856334308225bb5a00d5b4e11f1d5ea267b53cb
gorazdnina <ninagorazd@gmail.com>
2021-10-28 14:09:41 +0200

    Implemented String Controller

--- Lab2/src/main/java/com/example/Lab2/controller/StringController.java
+++ Lab2/src/main/java/com/example/Lab2/controller/StringController.java
@@ -1,4 +1,21 @@
 package com.example.Lab2.controller;
 
+import com.example.Lab2.dto.CountRequest;
+import com.example.Lab2.dto.CountResponse;
+import com.example.Lab2.service.StringService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StringController {
+    @Autowired
+    private StringService stringService;
+    @GetMapping("/request")
+    public ResponseEntity<CountResponse> getController(@RequestBody CountRequest countRequest) {
+        return ResponseEntity.ok(stringService.countString(countRequest));
+    }
+
 }
