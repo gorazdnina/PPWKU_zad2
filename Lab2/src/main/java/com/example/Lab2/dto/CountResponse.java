dd9ca611ddb7494c33586632ea16e6f0bafcec56
gorazdnina <ninagorazd@gmail.com>
2021-10-28 13:43:33 +0200

    Added variables in CountResponse

--- Lab2/src/main/java/com/example/Lab2/dto/CountResponse.java
+++ Lab2/src/main/java/com/example/Lab2/dto/CountResponse.java
@@ -1,4 +1,17 @@
 package com.example.Lab2.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CountResponse {
+    private Long uppercaseLetters;
+    private Long lowercaseLetters;
+    private Long specialSigns;
+    private Long numOfAppearances;
 }
