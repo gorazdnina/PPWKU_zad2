768a1e2ebeb8dbd3084050bf21448fc5dc6f3d06
gorazdnina <ninagorazd@gmail.com>
2021-10-28 13:36:42 +0200

    Added CountRequest

--- Lab2/src/main/java/com/example/Lab2/dto/CountRequest.java
+++ Lab2/src/main/java/com/example/Lab2/dto/CountRequest.java
@@ -1,4 +1,13 @@
 package com.example.Lab2.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CountRequest {
+    private String string;
+    private String expresion;
 }
