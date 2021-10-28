3826d926b94bdddad99fea3fe56a5b3d8c39e953
gorazdnina <ninagorazd@gmail.com>
2021-10-28 14:09:11 +0200

    Implemented countString method

--- Lab2/src/main/java/com/example/Lab2/service/StringService.java
+++ Lab2/src/main/java/com/example/Lab2/service/StringService.java
@@ -1,4 +1,21 @@
 package com.example.Lab2.service;
 
+import com.example.Lab2.dto.CountRequest;
+import com.example.Lab2.dto.CountResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StringService {
+    public CountResponse countString(CountRequest countRequest) {
+        String toCount = countRequest.getString();
+        CountResponse countResponse = new CountResponse();
+        for (int k = 0; k < toCount.length(); k++) {
+            if (Character.isUpperCase(toCount.charAt(k))) countResponse.setUppercaseLetters(countResponse.getUppercaseLetters()+1);
+            else if (Character.isLowerCase(toCount.charAt(k))) countResponse.setLowercaseLetters(countResponse.getLowercaseLetters()+1);
+            else if (Character.isDigit(toCount.charAt(k))) countResponse.setDigits(countResponse.getDigits()+1);
+            else if (!Character.isWhitespace(toCount.charAt(k))) countResponse.setSpecialSigns(countResponse.getSpecialSigns()+1);
+        }
+        countResponse.setNumOfOccurrences((long) (toCount.split(countRequest.getExpresion(), -1).length-1));
+        return countResponse;
+    }
 }
